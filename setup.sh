8ab78aa9ac6b9f280995d3dfe84f8ea5e324e552
Adrian Murawski <adrianmurawski79@gmail.com>
2024-06-24 13:38:39 +0200

    added triggers, small fixes in insert, setup

--- setup.sh
+++ setup.sh
@@ -32,6 +32,8 @@ else
     docker cp sql_scripts/triggers.sql "${CONTAINER_NAME}":/triggers
     docker exec horses-database /bin/sh -c 'mysql -u root -ppassword </triggers'
 
+    sleep 10
+
     echo "Inserting data..."
     docker cp sql_scripts/insert_data.sql "${CONTAINER_NAME}":/initialize.sql
     docker exec horses-database /bin/sh -c 'mysql -u root -ppassword </initialize.sql'
